a3a8bca0a5d7e65c9ef3b9bd04992fe7346a1c45
roynegara <roybayunegara92@gmail.com>
2024-04-03 11:20:19 +0700

    success repair bug for register, bug noitf, and complete input, also added time for route to login after registered

--- src/pages/authentication/register.js
+++ src/pages/authentication/register.js
@@ -8,8 +8,13 @@ const Register = () => {
     name: "",
     email: "",
     password: "",
+    passwordRepeat: "",
+    role: "",
+    profilPictureUrl: "",
+    phoneNumber: "",
   });
-  const [errors, setErrors] = useState("");
+//   const [errors, setErrors] = useState("");
+  const [showNotification, setShowNotification] = useState('');
 
   const handleChange = (e) => {
     setFormData({
@@ -23,10 +28,16 @@ const Register = () => {
     try {
       const res = await registerUser(formData);
       console.log(res.data);
-      router.push("/authentication/login");
-    } catch (error) {
-      console.error(error);
-      setErrors(error.message);
+      //   router.push("/authentication/login");
+      setShowNotification('Successfully registered');
+      setTimeout(() => {
+        
+        router.push("/authentication/login");
+      }, 3000);
+    } catch (eror) {
+      console.error(eror);
+
+      setShowNotification(eror?.message);
     }
   };
 
@@ -46,7 +57,24 @@ const Register = () => {
           <label>Password:</label>
           <input type="password" name="password" value={formData.password} onChange={handleChange} />
         </div>
-        {errors && <p style={{ color: "red" }}>{errors}</p>}
+        <div>
+          <label>Password Repeat:</label>
+          <input type="password" name="passwordRepeat" value={formData.passwordRepeat} onChange={handleChange} />
+        </div>
+        <div>
+          <label>Role:</label>
+          <input type="text" name="role" value={formData.role} onChange={handleChange} />
+        </div>
+        <div>
+          <label>Profil Picture Url:</label>
+          <input type="text" name="profilPictureUrl" value={formData.profilPictureUrl} onChange={handleChange} />
+        </div>
+        <div>
+          <label>Phone Number:</label>
+          <input type="text" name="phoneNumber" value={formData.phoneNumber} onChange={handleChange} />
+        </div>
+
+        {showNotification && <p style={{ color: "red" }}>{showNotification}</p>}
         <button type="submit">Register</button>
       </form>
     </div>
@@ -54,3 +82,81 @@ const Register = () => {
 };
 
 export default Register;
+
+// import React, { useState } from "react";
+// import { useRouter } from "next/router";
+// import { registerUser } from "@/api/authentication";
+
+// const Register = () => {
+//   const router = useRouter();
+//   const [formData, setFormData] = useState({
+//     name: "",
+//     email: "",
+//     password: "",
+//     passwordRepeat: "",
+//     role: "",
+//     profilPictureUrl: "",
+//     phoneNumber: "",
+//   });
+//   const [errors, setErrors] = useState("");
+
+//   const handleChange = (e) => {
+//     setFormData({
+//       ...formData,
+//       [e.target.name]: e.target.value,
+//     });
+//   };
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     try {
+//       const res = await registerUser(formData);
+//       console.log(res.data);
+//       router.push("/authentication/login");
+//     } catch (error) {
+//       console.error(error.data.status);
+//       setErrors(error.message);
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <h1>Register</h1>
+//       <form onSubmit={handleSubmit}>
+//         <div>
+//           <label>Name:</label>
+//           <input type="text" name="name" value={formData.name} onChange={handleChange} />
+//         </div>
+//         <div>
+//           <label>Email:</label>
+//           <input type="email" name="email" value={formData.email} onChange={handleChange} />
+//         </div>
+//         <div>
+//           <label>Password:</label>
+//           <input type="password" name="password" value={formData.password} onChange={handleChange} />
+//         </div>
+//         <div>
+//           <label>Password Repeat:</label>
+//           <input type="password" name="passwordRepeat" value={formData.passwordRepeat} onChange={handleChange} />
+//         </div>
+//         <div>
+//           <label>Role:</label>
+//           <input type="text" name="role" value={formData.role} onChange={handleChange} />
+//         </div>
+//         <div>
+//           <label>Profil Picture Url:</label>
+//           <input type="text" name="profilPictureUrl" value={formData.profilPictureUrl} onChange={handleChange} />
+//         </div>
+//         <div>
+//           <label>Phone Number:</label>
+//           <input type="text" name="phoneNumber" value={formData.phoneNumber} onChange={handleChange} />
+//         </div>
+
+//         {errors && <p style={{ color: "red" }}>{errors}</p>}
+//         <button type="submit">Register</button>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default Register;
