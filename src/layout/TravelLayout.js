4a85d78e78bf4c82c98d50d70ee53561b2a4e2e8
roynegara <roybayunegara92@gmail.com>
2024-04-04 07:58:08 +0700

    add Footer

--- src/layout/TravelLayout.js
+++ src/layout/TravelLayout.js
@@ -1,10 +1,12 @@
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 export default function TravelLayout({ children }) {
     return (
         <div>
             <Navbar />
             <main>{children}</main>
+            <Footer />
         </div>
     );
 }
