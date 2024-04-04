349004a84d9460fe6d76ccfda99b5d24787456fc
roynegara <roybayunegara92@gmail.com>
2024-04-04 08:41:35 +0700

    add login and logout for not login appear login if already login appear logout

---
+++ src/pages/authentication/logout.js
@@ -0,0 +1,25 @@
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
+import { logoutUser } from "@/api/authentication";
+
+export default function Logout() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleLogout = async () => {
+      try {
+        await logoutUser();
+        router.push("/authentication/login");
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    handleLogout();
+  }, []);
+
+  return (
+    <div>
+      <p>Logging out...</p>
+    </div>
+  );
+}
