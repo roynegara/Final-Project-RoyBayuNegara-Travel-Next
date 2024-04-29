1457f31bc39c52b7fdb1b791ddc6ecb5e32ab79e
roynegara <roybayunegara92@gmail.com>
2024-04-29 10:27:43 +0700

    succes for update image promo from file upload

--- src/components/PopupCreatePromo.js
+++ src/components/PopupCreatePromo.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { toast } from "sonner";
 import { useRouter } from "next/router";
@@ -11,6 +11,7 @@ const CreatePromo = (props) => {
   const [promo_code, setPromo_code] = useState("");
   const [promo_discount_price, setPromo_discount_price] = useState("");
   const [minimum_claim_price, setMinimum_claim_price] = useState("");
+  const [file, setFile] = useState('');
 
   const router = useRouter();
 
@@ -23,7 +24,7 @@ const CreatePromo = (props) => {
   };
 
   const handleImageUrlChange = (e) => {
-    setImageUrl(e.target.value);
+    setFile(e.target.files[0]);
   };
 
   const handleTerms_conditionChange = (e) => {
@@ -68,11 +69,54 @@ const CreatePromo = (props) => {
         toast.success(`${title} has been created`);
         props.createPromosData(); // Update promo data in parent component
         props.setTrigger(false);
+        setImageUrl(res?.data?.url);
       })
       .catch((err) => {
         toast.error(err.response?.data?.message);
       });
   };
+
+  useEffect(() => {
+    handleSubmit()
+  }, [imageUrl])
+
+  const handleUpload = () => {
+    if (!file) {
+      toast.warning("Please select an image");
+      return;
+    }
+    
+    
+        const formData = new FormData();
+        formData.append("image", file);
+    
+        const config = {
+          headers: {
+            "content-type": "multipart/form-data",
+            Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1pZnRhaGZhcmhhbkBnbWFpbC5jb20iLCJ1c2VySWQiOiI5NWE4MDNjMy1iNTFlLTQ3YTAtOTBkYi0yYzJmM2Y0ODE1YTkiLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE2Nzk4NDM0NDR9.ETsN6dCiC7isPReiQyHCQxya7wzj05wz5zruiFXLx0k`,
+            apiKey: "24405e01-fbc1-45a5-9f5a-be13afcd757c",
+          },
+        };
+    
+        axios
+          .post(
+            "https://travel-journal-api-bootcamp.do.dibimbing.id/api/v1/upload-image",
+            formData,
+            config
+          )
+          .then((res) => {
+            console.log(res);
+            setImageUrl(res.data.url);
+            // toast.success(res?.data?.message);
+           
+           
+    })
+          .catch((err) => {
+            console.log(err);
+            // toast.error(err?.response?.data?.message)
+          });
+      };
+
 
   return props.trigger ? (
     <div className="popup-create-promo-wrap">
@@ -85,7 +129,7 @@ const CreatePromo = (props) => {
           <input type="text" name="description" value={description} onChange={handleDescriptionChange} placeholder="Description" />
         </div>
         <div className="input-box-create-promos">
-          <input type="text" name="imageUrl" value={imageUrl} onChange={handleImageUrlChange} placeholder="Image Url" />
+          <input type="file" name="imageUrl"  onChange={handleImageUrlChange} placeholder="Image Url" />
         </div>
         <div className="input-box-create-promos">
           <input type="text" name="terms_condition" value={terms_condition} onChange={handleTerms_conditionChange} placeholder="Terms Condition" />
@@ -100,7 +144,7 @@ const CreatePromo = (props) => {
           <input type="text" name="minimum_claim_price" value={minimum_claim_price} onChange={handleMinimum_claim_priceChange} placeholder="Minimum Claim Price" />
         </div>
         <div className="btn-create-promos-popup">
-          <button onClick={handleSubmit}>Add Promo</button>
+          <button onClick={handleUpload}>Add Promo</button>
         </div>
         <span className="btn-close-popup-create-promo" onClick={() => props.setTrigger(false)}>&times;</span>
       </div>
